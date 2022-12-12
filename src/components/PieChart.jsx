e39c8499e32ee013c8bda57a244358014d30c3ed
harishpampana7 <harishpampana7675@gmail.com>
2022-12-12 13:37:42 +0530

    admin

---
+++ src/components/PieChart.jsx
@@ -0,0 +1,99 @@
+import { ResponsivePie } from "@nivo/pie";
+import { tokens } from "../theme";
+import { useTheme } from "@mui/material";
+import { mockPieData as data } from "../data/mockData";
+
+const PieChart = () => {
+  const theme = useTheme();
+  const colors = tokens(theme.palette.mode);
+  return (
+    <ResponsivePie
+        data={data}
+        theme={{
+          axis: {
+            domain: {
+              line: {
+                stroke: colors.grey[100],
+              },
+            },
+            legend: {
+              text: {
+                fill: colors.grey[100],
+              },
+            },
+            ticks: {
+              line: {
+                stroke: colors.grey[100],
+                strokeWidth: 1,
+              },
+              text: {
+                fill: colors.grey[100],
+              },
+            },
+          },
+          legends: {
+            text: {
+              fill: colors.grey[100],
+            },
+          },
+        }}
+        margin={{ top: 40, right: 80, bottom: 80, left: 80 }}
+        innerRadius={0.5}
+        padAngle={0.7}
+        cornerRadius={3}
+        activeOuterRadiusOffset={8}
+        colors={{ scheme: 'set1' }}
+        borderColor={{ theme: 'grid.line.stroke' }}
+        arcLinkLabelsSkipAngle={10}
+        arcLinkLabelsTextColor={{ from: 'color', modifiers: [] }}
+        arcLinkLabelsThickness={2}
+        arcLinkLabelsColor={{ from: 'color', modifiers: [] }}
+        enableArcLabels={false}
+        arcLabelsSkipAngle={10}
+        arcLabelsTextColor={{
+            from: 'color',
+            modifiers: [
+                [
+                    'darker',
+                    2
+                ]
+            ]
+        }}
+        motionConfig={{
+            mass: 1,
+            tension: 139,
+            friction: 9,
+            clamp: false,
+            precision: 0.01,
+            velocity: 0
+        }}
+        legends={[
+            {
+                anchor: 'bottom',
+                direction: 'row',
+                justify: false,
+                translateX: 0,
+                translateY: 56,
+                itemsSpacing: 0,
+                itemWidth: 100,
+                itemHeight: 18,
+                itemTextColor: '#999',
+                itemDirection: 'left-to-right',
+                itemOpacity: 1,
+                symbolSize: 18,
+                symbolShape: 'circle',
+                effects: [
+                    {
+                        on: 'hover',
+                        style: {
+                            itemTextColor: '#000'
+                        }
+                    }
+                ]
+            }
+        ]}
+    />
+  );
+};
+
+export default PieChart;
